b279a3ad87ae611f60ebd016642c79c900f839b5
areeb1899 <areebahmedwork@gmail.com>
2025-07-03 01:58:17 +0530

    backend update v5

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const convertRoutes = require("./routes/convertRoutes");
+const fs = require("fs");
+
+// Ensure uploads and converted directories exist
+const uploadDir = path.join(__dirname, "uploads");
+const convertedDir = path.join(__dirname, "converted");
+
+if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+if (!fs.existsSync(convertedDir)) fs.mkdirSync(convertedDir);
 
 const app = express();
 
